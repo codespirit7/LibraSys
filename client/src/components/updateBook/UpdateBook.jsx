5db3bcdfd0156adb7f60891dd1c60b5a8390dcbe
Ritesh Singh <itsriteshoff@gmail.com>
2023-05-24 19:15:12 +0530

    Update UpdateBook.jsx

--- client/src/components/updateBook/UpdateBook.jsx
+++ client/src/components/updateBook/UpdateBook.jsx
@@ -11,7 +11,7 @@ export default function AdminRegister({ book }) {
     e.preventDefault();
     try {
       const response = await fetch(
-        `http://localhost:5000/book/update/${book._id}`,
+        `https://libra-sys.onrender.com/book/update/${book._id}`,
         {
           method: "PUT",
           headers: {
